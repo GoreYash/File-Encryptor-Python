4b51d271889cedbac9cae8158645b7b56991fa88
Yash <yashgore0611@gmail.com>
2026-01-04 06:31:04 +0530

    Upgraded to password security

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
 from cryptography.fernet import Fernet
 import os
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+import base64
 
 def generate(path):
     if not os.path.exists(path) or (os.path.getsize(path) == 0):
         with open(path, 'wb') as file:
-                file.write(Fernet.generate_key())
-                print("Key created successfully")
-        return
-    else:
-         return
+            file.write(os.urandom(16))
+    with open(path, "rb") as file:
+        return file.read()
     
+def derive_key(password, salt):
+    password = password.encode()
+    kdf = PBKDF2HMAC(length=32,salt=salt,iterations=100000,algorithm=hashes.SHA256())
+    return base64.urlsafe_b64encode(kdf.derive(password))
+
 def encryptFile(f, key):
     with open(f,'rb') as file:
         data = file.read()
         fer = Fernet(key)
         data = fer.encrypt(data)
-        
-
+    
     with open(f, 'wb') as file:
         file.write(data)
+    return True
 
 def decryptFile(f, key):
     try:
@@ -31,19 +37,20 @@ def decryptFile(f, key):
 
         with open(f, 'wb') as file:
             file.write(data)
+        return True
     except Exception:
-        print("The file might not be encrypted")
+        return False
 
-p = 'secret.key' 
-generate(p)
-with open(p, "rb") as file:
-    key = file.read()
-
-print("Welcome!!")
-f = input("Please enter the file path: ")
-i = int(input("1) Encrypt\n2) Decrypt\nEnter a choice: "))
-match(i):
-    case 1:
-        encryptFile(f,key)
-    case 2:
-        decryptFile(f,key)
+p = 'X.salt' 
+salt = generate(p)
+password = input("Enter your password: ")
+key = derive_key(password,salt)
+if key:
+    print("Welcome!!")
+    f = input("Please enter the file path: ")
+    i = int(input("1) Encrypt\n2) Decrypt\nEnter a choice: "))
+    match(i):
+        case 1:
+            encryptFile(f,key)
+        case 2:
+            decryptFile(f,key)
